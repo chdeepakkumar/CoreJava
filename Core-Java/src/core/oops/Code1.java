5ccb789a2554a762daf7dcd53ff43062b430cd76
Ch Deepak Kumar <deepakkumar.ch@outlook.com>
2023-06-28 22:55:18 +0530

    Add files via upload

---
+++ Core-Java/src/core/oops/Code1.java
@@ -0,0 +1,10 @@
+package core.oops;
+
+public class Code1 {
+	// 4 oops concepts
+	// 1. Encapsulation - Wrapping-up similar information in class (capsul)
+	// 2. Abstraction - Revealing only the required information (the methods, variables) 
+	// 3. Inheritance - Creating parent-child relation between classes ("Is a" relation)
+	// 4. Polymorphism - Creating multiple copies of same methods and constructors
+	
+}
